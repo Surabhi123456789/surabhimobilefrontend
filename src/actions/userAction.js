4482dfbe0a333863fbe1ea214efa91df64c87513
abhaycoderarmy <2320403202@stu.manit.ac.in>
2025-06-24 12:14:36 +0530

    added true

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -88,7 +88,9 @@ export const loadUser = () => async (dispatch) => {
   try {
     dispatch({ type: LOAD_USER_REQUEST });
 
-    const { data } = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/v1/me`);
+    const { data } = await axios.get(`${baseURL}/api/v1/me`, {
+      withCredentials: true, 
+    });
 
     dispatch({ type: LOAD_USER_SUCCESS, payload: data.user });
     // Load user's cart after loading user data
